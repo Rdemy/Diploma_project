99ab252609e6808c203f98d345cfd3714c004db9
Rdemy <zenimax48@gmail.com>
2021-10-06 22:15:22 +0500

    Парсинг по страницам

--- spiders/katalog_parser.py
+++ spiders/katalog_parser.py
@@ -5,13 +5,17 @@ class EKatalogPartsSpider(scrapy.Spider):
     name = 'parts'
     start_urls = ['https://www.kns.ru/catalog/komplektuyuschie/']
 
+
     def parse(self, response):
         details = response.xpath('//*[@class = "goods-list-item mx-auto"]')
         for category in details:
             yield {
                     'name': category.xpath('./*[@class="position-relative my-1 name-cont pr-xl-0"]//span/text()').get(),
-                    'price': category.xpath('./*[@class="block-price"]//span/text()').get().replace('\xa0',''),
+                    'price': category.xpath('./*[@class="block-price"]//span/text()').get().replace('\xa0', ''),
                     'description': category.xpath('./*[@class = "position-relative my-1 name-cont pr-xl-0"]//div/div/text()').get(),
                     'rating': category.xpath('.//div[@class="col-12 col-sm-6 text-md-right block-info"]//div//meta[1]/@content').get(),
                     'commentary': category.xpath('.//div[@class="col-12 col-sm-6 text-md-right block-info"]//div//meta[2]/@content').get()
             }
+        for pages in response.xpath('//a[@class="page-link"]//@href')[1:5]:
+            yield response.follow(pages, callback=self.parse)
+            
