b9ef14ad7d1f2da68fcccf1a49d6c3730941da48
Rdemy <zenimax48@gmail.com>
2021-10-13 17:28:39 +0300

    KNS parser

--- items.py
+++ items.py
@@ -4,9 +4,19 @@
 # https://docs.scrapy.org/en/latest/topics/items.html
 
 import scrapy
+from scrapy.loader import ItemLoader
+from itemloaders.processors import TakeFirst, MapCompose
+from w3lib.html import remove_tags
 
 
 class DnsparserItem(scrapy.Item):
-    product_name = scrapy.Field()
-    product_price = scrapy.Field()
-    pass
+    name = scrapy.Field(input_processor=MapCompose(remove_tags), output_processor=TakeFirst())
+    price = scrapy.Field(input_processor=MapCompose(remove_tags), output_processor=TakeFirst())
+    rating = scrapy.Field(input_processor=MapCompose(remove_tags), output_processor=TakeFirst())
+    commentary = scrapy.Field(input_processor=MapCompose(remove_tags), output_processor=TakeFirst())
+    Attrib_1 = scrapy.Field(input_processor=MapCompose(remove_tags), output_processor=TakeFirst())
+    Attrib_2 = scrapy.Field(input_processor=MapCompose(remove_tags), output_processor=TakeFirst())
+    Attrib_3 = scrapy.Field(input_processor=MapCompose(remove_tags), output_processor=TakeFirst())
+    Attrib_3 = scrapy.Field(input_processor=MapCompose(remove_tags), output_processor=TakeFirst())
+    Attrib_4 = scrapy.Field(input_processor=MapCompose(remove_tags), output_processor=TakeFirst())
+    Attrib_5 = scrapy.Field(input_processor=MapCompose(remove_tags), output_processor=TakeFirst())
--- spiders/katalog_parser.py
+++ spiders/katalog_parser.py
@@ -1,21 +1,33 @@
+from itemloaders import processors
 import scrapy
+from scrapy import selector
+from dnsparser.items import DnsparserItem
+from scrapy.loader import ItemLoader
 
 
 class EKatalogPartsSpider(scrapy.Spider):
     name = 'parts'
-    start_urls = ['https://www.kns.ru/catalog/komplektuyuschie/']
-
+    start_urls = ['https://www.kns.ru/catalog/komplektuyuschie/protsessory/']
 
     def parse(self, response):
-        details = response.xpath('//*[@class = "goods-list-item mx-auto"]')
-        for category in details:
-            yield {
-                    'name': category.xpath('./*[@class="position-relative my-1 name-cont pr-xl-0"]//span/text()').get(),
-                    'price': category.xpath('./*[@class="block-price"]//span/text()').get().replace('\xa0', ''),
-                    'description': category.xpath('./*[@class = "position-relative my-1 name-cont pr-xl-0"]//div/div/text()').get(),
-                    'rating': category.xpath('.//div[@class="col-12 col-sm-6 text-md-right block-info"]//div//meta[1]/@content').get(),
-                    'commentary': category.xpath('.//div[@class="col-12 col-sm-6 text-md-right block-info"]//div//meta[2]/@content').get()
-            }
-        for pages in response.xpath('//a[@class="page-link"]//@href')[1:5]:
+        for category in response.xpath('//a[@class = "name d-block"]//@href'):
+            yield response.follow(category, callback=self.info_parse)
+
+        for pages in response.xpath('//*[@class="page-link"]//@href')[1]:
             yield response.follow(pages, callback=self.parse)
-            
+
+    def info_parse(self, response):
+        info_link = response.xpath('//*[@class = "cont bg-white p-1"]')
+        for proccesors in info_link:
+            l = ItemLoader(item=DnsparserItem(), selector=proccesors)
+            l.add_xpath('name', '//h1[@itemprop = "name"]')
+            l.add_xpath('price', '//span[@class = "price-val"]')
+            l.add_xpath('rating', '//div[@class = "aggregate-rating"]//meta[1]/@content')
+            l.add_xpath('commentary', '//div[@class = "aggregate-rating"]//meta[2]/@content')
+            l.add_xpath('Attrib_1', '//div[@data-id = "1416601096"]')
+            l.add_xpath('Attrib_2', '//div[@data-id = "1416601099"]')
+            l.add_xpath('Attrib_3', '//div[@data-id = "1416604950"]')
+            l.add_xpath('Attrib_4', '//div[@data-id = "1416605706"]')
+            l.add_xpath('Attrib_5', '//div[@data-id = "1416601097"]')
+            yield l.load_item()
+
