3126bd23f6595477bcb6664de1f3adff36f90f8f
Rdemy <zenimax48@gmail.com>
2021-10-14 11:50:21 +0300

    Parts parsers

--- items.py
+++ items.py
@@ -10,13 +10,49 @@ from w3lib.html import remove_tags
 
 
 class DnsparserItem(scrapy.Item):
+# Basic attributes
     name = scrapy.Field(input_processor=MapCompose(remove_tags), output_processor=TakeFirst())
     price = scrapy.Field(input_processor=MapCompose(remove_tags), output_processor=TakeFirst())
     rating = scrapy.Field(input_processor=MapCompose(remove_tags), output_processor=TakeFirst())
     commentary = scrapy.Field(input_processor=MapCompose(remove_tags), output_processor=TakeFirst())
-    Attrib_1 = scrapy.Field(input_processor=MapCompose(remove_tags), output_processor=TakeFirst())
-    Attrib_2 = scrapy.Field(input_processor=MapCompose(remove_tags), output_processor=TakeFirst())
-    Attrib_3 = scrapy.Field(input_processor=MapCompose(remove_tags), output_processor=TakeFirst())
-    Attrib_3 = scrapy.Field(input_processor=MapCompose(remove_tags), output_processor=TakeFirst())
-    Attrib_4 = scrapy.Field(input_processor=MapCompose(remove_tags), output_processor=TakeFirst())
-    Attrib_5 = scrapy.Field(input_processor=MapCompose(remove_tags), output_processor=TakeFirst())
+
+# Proceccors attributes
+    cores = scrapy.Field(input_processor=MapCompose(remove_tags), output_processor=TakeFirst())
+    frequency = scrapy.Field(input_processor=MapCompose(remove_tags), output_processor=TakeFirst())
+    waranty_procs = scrapy.Field(input_processor=MapCompose(remove_tags), output_processor=TakeFirst())
+    proc_memory_type = scrapy.Field(input_processor=MapCompose(remove_tags), output_processor=TakeFirst())
+    tech_process = scrapy.Field(input_processor=MapCompose(remove_tags), output_processor=TakeFirst())
+
+# Motherboard attributes
+    socket = scrapy.Field(input_processor=MapCompose(remove_tags), output_processor=TakeFirst())
+    chipset = scrapy.Field(input_processor=MapCompose(remove_tags), output_processor=TakeFirst())
+    memory_board = scrapy.Field(input_processor=MapCompose(remove_tags), output_processor=TakeFirst())
+    memory_slots = scrapy.Field(input_processor=MapCompose(remove_tags), output_processor=TakeFirst())
+    memory_value = scrapy.Field(input_processor=MapCompose(remove_tags), output_processor=TakeFirst())
+    waranty_board = scrapy.Field(input_processor=MapCompose(remove_tags), output_processor=TakeFirst())
+
+# Videocard attributes
+    brand = scrapy.Field(input_processor=MapCompose(remove_tags), output_processor=TakeFirst())
+    video_memory_type = scrapy.Field(input_processor=MapCompose(remove_tags), output_processor=TakeFirst())
+    video_memory_value = scrapy.Field(input_processor=MapCompose(remove_tags), output_processor=TakeFirst())
+    dram = scrapy.Field(input_processor=MapCompose(remove_tags), output_processor=TakeFirst())
+    max_video_resolution = scrapy.Field(input_processor=MapCompose(remove_tags), output_processor=TakeFirst())
+    waranty_video = scrapy.Field(input_processor=MapCompose(remove_tags), output_processor=TakeFirst())
+    freq_video = scrapy.Field(input_processor=MapCompose(remove_tags), output_processor=TakeFirst())
+
+# RAM attributes
+    RAM_memory_type = scrapy.Field(input_processor=MapCompose(remove_tags), output_processor=TakeFirst())
+    RAM_memory_value = scrapy.Field(input_processor=MapCompose(remove_tags), output_processor=TakeFirst())
+    waranty_RAM = scrapy.Field(input_processor=MapCompose(remove_tags), output_processor=TakeFirst())
+
+# HDD attributes
+    HDD_model = scrapy.Field(input_processor=MapCompose(remove_tags), output_processor=TakeFirst())
+    speed = scrapy.Field(input_processor=MapCompose(remove_tags), output_processor=TakeFirst())
+    HDD_memory_value = scrapy.Field(input_processor=MapCompose(remove_tags), output_processor=TakeFirst())
+    waranty_HDD = scrapy.Field(input_processor=MapCompose(remove_tags), output_processor=TakeFirst())
+
+# SSD attributes
+    SDD_model = scrapy.Field(input_processor=MapCompose(remove_tags), output_processor=TakeFirst())
+    SSD_speed = scrapy.Field(input_processor=MapCompose(remove_tags), output_processor=TakeFirst())
+    SSD_memory_value = scrapy.Field(input_processor=MapCompose(remove_tags), output_processor=TakeFirst())
+    waranty_SSD = scrapy.Field(input_processor=MapCompose(remove_tags), output_processor=TakeFirst())
--- spiders/katalog_parser.py
+++ spiders/katalog_parser.py
@@ -5,16 +5,20 @@ from dnsparser.items import DnsparserItem
 from scrapy.loader import ItemLoader
 
 
-class EKatalogPartsSpider(scrapy.Spider):
-    name = 'parts'
-    start_urls = ['https://www.kns.ru/catalog/komplektuyuschie/protsessory/']
+class ProceccorsSpider(scrapy.Spider):
+    name = 'procers'
+    start_urls = ['https://www.kns.ru/catalog/komplektuyuschie/']
+    pages_count = 1
 
-    def parse(self, response):
-        for category in response.xpath('//a[@class = "name d-block"]//@href'):
-            yield response.follow(category, callback=self.info_parse)
+    def start_requests(self):
+        for page in range(1, 1 + self.pages_count):
+            url = f'https://www.kns.ru/catalog/komplektuyuschie/protsessory/page{page}/'
+            yield scrapy.Request(url, callback=self.parse_pages)
 
-        for pages in response.xpath('//*[@class="page-link"]//@href')[1]:
-            yield response.follow(pages, callback=self.parse)
+    def parse_pages(self, response):
+        for href in response.xpath('//a[@class = "name d-block"]//@href').getall():
+            url = response.urljoin(href)
+            yield scrapy.Request(url, callback=self.info_parse)
 
     def info_parse(self, response):
         info_link = response.xpath('//*[@class = "cont bg-white p-1"]')
@@ -24,10 +28,163 @@ class EKatalogPartsSpider(scrapy.Spider):
             l.add_xpath('price', '//span[@class = "price-val"]')
             l.add_xpath('rating', '//div[@class = "aggregate-rating"]//meta[1]/@content')
             l.add_xpath('commentary', '//div[@class = "aggregate-rating"]//meta[2]/@content')
-            l.add_xpath('Attrib_1', '//div[@data-id = "1416601096"]')
-            l.add_xpath('Attrib_2', '//div[@data-id = "1416601099"]')
-            l.add_xpath('Attrib_3', '//div[@data-id = "1416604950"]')
-            l.add_xpath('Attrib_4', '//div[@data-id = "1416605706"]')
-            l.add_xpath('Attrib_5', '//div[@data-id = "1416601097"]')
+            l.add_xpath('cores', '//div[@data-id = "1416601096"]')
+            l.add_xpath('frequency', '//div[@data-id = "1416601099"]')
+            l.add_xpath('waranty_procs', '//div[@data-id = "1416604950"]')
+            l.add_xpath('proc_memory_type', '//div[@data-id = "1416605706"]')
+            l.add_xpath('tech_process', '//div[@data-id = "1416601097"]')
             yield l.load_item()
 
+
+class BoardSpider(scrapy.Spider):
+    name = 'motherboards'
+    start_urls = ['https://www.kns.ru/catalog/komplektuyuschie/']
+    pages_count = 1
+
+    def start_requests(self):
+        for page in range(1, 1 + self.pages_count):
+            url = f'https://www.kns.ru/catalog/komplektuyuschie/materinskie-platy/page{page}/'
+            yield scrapy.Request(url, callback=self.parse_pages)
+
+    def parse_pages(self, response):
+        for href in response.xpath('//a[@class = "name d-block"]//@href').getall():
+            url = response.urljoin(href)
+            yield scrapy.Request(url, callback=self.info_parse)
+
+    def info_parse(self, response):
+        info_link = response.xpath('//*[@class = "cont bg-white p-1"]')
+        for proccesors in info_link:
+            l = ItemLoader(item=DnsparserItem(), selector=proccesors)
+            l.add_xpath('name', '//h1[@itemprop = "name"]')
+            l.add_xpath('price', '//span[@class = "price-val"]')
+            l.add_xpath('rating', '//div[@class = "aggregate-rating"]//meta[1]/@content')
+            l.add_xpath('commentary', '//div[@class = "aggregate-rating"]//meta[2]/@content')
+            l.add_xpath('socket', '//div[@data-id = "1417701259"]')
+            l.add_xpath('chipset', '//div[@data-id = "1417701267"]')
+            l.add_xpath('memory_board', '//div[@data-id = "1417701270"]')
+            l.add_xpath('memory_slots', '//div[@data-id = "1417701274"]')
+            l.add_xpath('memory_value', '//div[@data-id = "1417701278"]')
+            l.add_xpath('waranty_board', '//div[@data-id = "1417704950"]')
+            yield l.load_item()
+
+
+class VideoSpider(scrapy.Spider):
+    name = 'videocards'
+    start_urls = ['https://www.kns.ru/catalog/komplektuyuschie/']
+    pages_count = 1
+
+    def start_requests(self):
+        for page in range(1, 1 + self.pages_count):
+            url = f'https://www.kns.ru/catalog/komplektuyuschie/videokarty/page{page}/'
+            yield scrapy.Request(url, callback=self.parse_pages)
+
+    def parse_pages(self, response):
+        for href in response.xpath('//a[@class = "name d-block"]//@href').getall():
+            url = response.urljoin(href)
+            yield scrapy.Request(url, callback=self.info_parse)
+
+    def info_parse(self, response):
+        info_link = response.xpath('//*[@class = "cont bg-white p-1"]')
+        for proccesors in info_link:
+            l = ItemLoader(item=DnsparserItem(), selector=proccesors)
+            l.add_xpath('name', '//h1[@itemprop = "name"]')
+            l.add_xpath('price', '//span[@class = "price-val"]')
+            l.add_xpath('rating', '//div[@class = "aggregate-rating"]//meta[1]/@content')
+            l.add_xpath('commentary', '//div[@class = "aggregate-rating"]//meta[2]/@content')
+            l.add_xpath('brand', '//div[@data-id = "1421001146"]')
+            l.add_xpath('video_memory_type', '//div[@data-id = "1421001156"]')
+            l.add_xpath('video_memory_value', '//div[@data-id = "1421001155"]')
+            l.add_xpath('dram', '//div[@data-id = "1421001157"]')
+            l.add_xpath('max_video_resolution', '//div[@data-id = "1421001152"]')
+            l.add_xpath('waranty_video', '//div[@data-id = "1416704950"]')
+            l.add_xpath('freq_video', '//div[@data-id = "1421001158"]')
+            yield l.load_item()
+
+
+class RAMSpider(scrapy.Spider):
+    name = 'RAM'
+    start_urls = ['https://www.kns.ru/catalog/komplektuyuschie/']
+    pages_count = 1
+
+    def start_requests(self):
+        for page in range(1, 1 + self.pages_count):
+            url = f'https://www.kns.ru/catalog/komplektuyuschie/pamyat/page{page}/'
+            yield scrapy.Request(url, callback=self.parse_pages)
+
+    def parse_pages(self, response):
+        for href in response.xpath('//a[@class = "name d-block"]//@href').getall():
+            url = response.urljoin(href)
+            yield scrapy.Request(url, callback=self.info_parse)
+
+    def info_parse(self, response):
+        info_link = response.xpath('//*[@class = "cont bg-white p-1"]')
+        for proccesors in info_link:
+            l = ItemLoader(item=DnsparserItem(), selector=proccesors)
+            l.add_xpath('name', '//h1[@itemprop = "name"]')
+            l.add_xpath('price', '//span[@class = "price-val"]')
+            l.add_xpath('rating', '//div[@class = "aggregate-rating"]//meta[1]/@content')
+            l.add_xpath('commentary', '//div[@class = "aggregate-rating"]//meta[2]/@content')
+            l.add_xpath('RAM_memory_type', '//div[@data-id = "1418601123"]')
+            l.add_xpath('RAM_memory_value', '//div[@data-id = "1418605199"]')
+            l.add_xpath('waranty_RAM', '//div[@data-id = "1418604950"]')
+            yield l.load_item()
+
+
+class HDDSpider(scrapy.Spider):
+    name = 'HDD'
+    start_urls = ['https://www.kns.ru/catalog/komplektuyuschie/']
+    pages_count = 1
+
+    def start_requests(self):
+        for page in range(1, 1 + self.pages_count):
+            url = f'https://www.kns.ru/catalog/komplektuyuschie/zhestkie-diski/page{page}/'
+            yield scrapy.Request(url, callback=self.parse_pages)
+
+    def parse_pages(self, response):
+        for href in response.xpath('//a[@class = "name d-block"]//@href').getall():
+            url = response.urljoin(href)
+            yield scrapy.Request(url, callback=self.info_parse)
+
+    def info_parse(self, response):
+        info_link = response.xpath('//*[@class = "cont bg-white p-1"]')
+        for proccesors in info_link:
+            l = ItemLoader(item=DnsparserItem(), selector=proccesors)
+            l.add_xpath('name', '//h1[@itemprop = "name"]')
+            l.add_xpath('price', '//span[@class = "price-val"]')
+            l.add_xpath('rating', '//div[@class = "aggregate-rating"]//meta[1]/@content')
+            l.add_xpath('commentary', '//div[@class = "aggregate-rating"]//meta[2]/@content')
+            l.add_xpath('HDD_model', '//div[@data-id = "1419304831"]')
+            l.add_xpath('speed', '//div[@data-id = "1419301244"]')
+            l.add_xpath('HDD_memory_value', '//div[@data-id = "1419301233"]')
+            l.add_xpath('waranty_HDD', '//div[@data-id = "1416704950"]')
+            yield l.load_item()
+
+
+class SDDSpider(scrapy.Spider):
+    name = 'SSD'
+    start_urls = ['https://www.kns.ru/catalog/komplektuyuschie/']
+    pages_count = 1
+
+    def start_requests(self):
+        for page in range(1, 1 + self.pages_count):
+            url = f'https://www.kns.ru/catalog/komplektuyuschie/ssd/page{page}/'
+            yield scrapy.Request(url, callback=self.parse_pages)
+
+    def parse_pages(self, response):
+        for href in response.xpath('//a[@class = "name d-block"]//@href').getall():
+            url = response.urljoin(href)
+            yield scrapy.Request(url, callback=self.info_parse)
+
+    def info_parse(self, response):
+        info_link = response.xpath('//*[@class = "cont bg-white p-1"]')
+        for proccesors in info_link:
+            l = ItemLoader(item=DnsparserItem(), selector=proccesors)
+            l.add_xpath('name', '//h1[@itemprop = "name"]')
+            l.add_xpath('price', '//span[@class = "price-val"]')
+            l.add_xpath('rating', '//div[@class = "aggregate-rating"]//meta[1]/@content')
+            l.add_xpath('commentary', '//div[@class = "aggregate-rating"]//meta[2]/@content')
+            l.add_xpath('SDD_model', '//div[@data-id = "44622004831"]')
+            l.add_xpath('SSD_speed', '//div[@data-id = "44622001336"]')
+            l.add_xpath('SSD_memory_value', '//div[@data-id = "44622001233"]')
+            l.add_xpath('waranty_SSD', '//div[@data-id = "1416704950"]')
+            yield l.load_item()
